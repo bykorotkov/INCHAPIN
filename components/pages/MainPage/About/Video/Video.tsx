070414cb942cfaf704f774c599e0c96cf591aef8
Igor Korotkov <bykorotkov@gmail.com>
2023-10-22 22:15:35 +0400

    IK:video fix

--- components/pages/MainPage/About/Video/Video.tsx
+++ components/pages/MainPage/About/Video/Video.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import classes from "./Video.module.scss"
 import { useAppDispatch } from "@/hooks/redux"
 import { openModal } from "@/store/reducers/modalSlice"
@@ -6,8 +6,30 @@ import useWindowSize from "@/hooks/useWindowSize"
 
 const Video = () => {
     const dispatch = useAppDispatch()
-    const [isOpen, setIsOpen] = useState()
+    const [isOpen, setIsOpen] = useState(false)
     const { width } = useWindowSize()
+
+    const openVideo = () => {
+        setIsOpen(true)
+    }
+
+    const closeVideo = () => {
+        setIsOpen(false)
+    }
+    const handleVideoLoadedMetadata = () => {
+        const videoElement = document.getElementById("VideoContainer")
+        if (videoElement) {
+            videoElement.requestFullscreen()
+            videoElement.addEventListener("fullscreenchange", handleFullscreenChange)
+        }
+    }
+
+    const handleFullscreenChange = () => {
+        const videoElement = document.getElementById("VideoContainer") as HTMLVideoElement
+        if (videoElement && document.fullscreenElement !== videoElement) {
+            closeVideo()
+        }
+    }
 
     return (
         <div className={classes.VideoBlock}>
@@ -16,21 +38,25 @@ const Video = () => {
                 <span>1:25 минут</span>
             </div>
             <button
-                onClick={() => dispatch(openModal("VideoForm"))}
+                onClick={width > 768 ? () => dispatch(openModal("VideoForm")) : openVideo}
                 className={classes.Video}
             >
                 <div className={classes.Effect}></div>
             </button>
             {isOpen && (
-                <video
-                    controls
-                    id={"mobileVideo"}
-                >
-                    <source
-                        src="/video.mp4"
-                        type="video/mp4"
-                    />
-                </video>
+                <div id="VideoContainer">
+                    <video
+                        controls
+                        id={"mobileVideo"}
+                        className={classes.mobileVideo}
+                        onLoadedMetadata={handleVideoLoadedMetadata}
+                    >
+                        <source
+                            src="/video.mp4"
+                            type="video/mp4"
+                        />
+                    </video>
+                </div>
             )}
         </div>
     )
