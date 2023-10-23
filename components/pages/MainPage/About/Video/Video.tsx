68b126e3bc8695858c0e16567b0491bb8ab5722d
Igor Korotkov <bykorotkov@gmail.com>
2023-10-23 16:56:57 +0400

    IK: video fix

--- components/pages/MainPage/About/Video/Video.tsx
+++ components/pages/MainPage/About/Video/Video.tsx
@@ -3,15 +3,29 @@ import classes from "./Video.module.scss"
 import { useAppDispatch } from "@/hooks/redux"
 import { openModal } from "@/store/reducers/modalSlice"
 import useWindowSize from "@/hooks/useWindowSize"
+import { is } from "immutable"
 
 const Video = () => {
     const dispatch = useAppDispatch()
     const [isOpen, setIsOpen] = useState<boolean>(false)
     const { width } = useWindowSize()
+    const videoRef = useRef<HTMLVideoElement>(null)
 
     const openVideo = () => {
         setIsOpen(true)
+
+        const videoElement = document.getElementById("mobileVideo") as any
+        if (videoElement) {
+            videoElement.style.display = "block"
+        }
     }
+
+    useEffect(() => {
+        if (isOpen && videoRef.current) {
+            videoRef.current.play()
+            videoRef.current.volume = 0.1
+        }
+    }, [isOpen])
 
     const closeVideo = () => {
         setIsOpen(false)
@@ -47,19 +61,19 @@ const Video = () => {
             >
                 <div className={classes.Effect}></div>
             </button>
-            {isOpen && (
-                <video
-                    controls
-                    id={"mobileVideo"}
-                    className={classes.mobileVideo}
-                    onLoadedMetadata={handleVideoLoadedMetadata}
-                >
-                    <source
-                        src="/video.mp4"
-                        type="video/mp4"
-                    />
-                </video>
-            )}
+            <video
+                controls
+                id={"mobileVideo"}
+                className={classes.mobileVideo}
+                onLoadedMetadata={handleVideoLoadedMetadata}
+                onChange={openVideo}
+                ref={videoRef}
+            >
+                <source
+                    src="/video.mp4"
+                    type="video/mp4"
+                />
+            </video>
         </div>
     )
 }
